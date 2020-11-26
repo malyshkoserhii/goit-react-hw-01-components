ad060b95f0c25af98243b4e1fc8c76aa144e2c0e
Malyshko Serhii <malyshkosergiy@gmail.com>
2020-11-26 17:05:38 +0200

    task 2 has been completed without css

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
-import Profile from "./components/Profile";
-import user from "./data/user.json";
+import Profile from './components/Profile/Profile';
+import StatisticalTitle from './components/StatisticalTitle/StatisticalTitle';
+import StatisticalList from './components/StatisticalList/StatisticalList';
+import user from './data/user.json';
+import statisticalData from './data/statistical-data.json';
 
 const App = () => {
   return (
-    <div>
-      <Profile
-        name={user.name}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
-    </div>
+    <>
+      <div>
+        <Profile
+          name={user.name}
+          tag={user.tag}
+          location={user.location}
+          avatar={user.avatar}
+          stats={user.stats}
+        />
+      </div>
+      <div>
+        <StatisticalTitle title="Upload Stats">
+          <StatisticalList stats={statisticalData} />
+        </StatisticalTitle>
+      </div>
+    </>
   );
 };
 
